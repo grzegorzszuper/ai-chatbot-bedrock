3e40b3810cdae4c95ace744b6bf63a92caaaebbb
Grzesiek <grzegorzszuper2@gmail.com>
2025-08-07 20:12:45 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,13 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_iam_role" "lambda_bedrock_role" {
-  name = "lambda-bedrock-role"
-
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
       Principal = {
         Service = "lambda.amazonaws.com"
       }
@@ -17,52 +16,22 @@ resource "aws_iam_role" "lambda_bedrock_role" {
   })
 }
 
-resource "aws_iam_role_policy" "bedrock_invoke_policy" {
-  name = "bedrock-invoke-policy"
-  role = aws_iam_role.lambda_bedrock_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = ["bedrock:InvokeModel"],
-        Resource = "*"
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_lambda_function" "chatbot_lambda" {
-  function_name = "ai-chatbot-lambda"
-  role          = aws_iam_role.lambda_bedrock_role.arn
-  handler       = "handler.lambda_handler"
-  runtime       = "python3.12"
-  filename      = "lambda.zip"
-  timeout       = 30
+  filename         = "lambda.zip"
+  function_name    = "chatbotHandler"
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
   source_code_hash = filebase64sha256("lambda.zip")
 }
 
-resource "aws_lambda_permission" "allow_api_gateway" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.chatbot_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.chatbot_api.execution_arn}/*/*"
-}
-
 resource "aws_api_gateway_rest_api" "chatbot_api" {
-  name        = "chatbot-api"
-  description = "API Gateway for AI chatbot using Claude 3 and Lambda"
+  name = "chatbot-api"
 }
 
 resource "aws_api_gateway_resource" "chatbot_resource" {
@@ -79,7 +48,7 @@ resource "aws_api_gateway_method" "chatbot_post" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "chatbot_integration" {
+resource "aws_api_gateway_integration" "chatbot_post_integration" {
   rest_api_id             = aws_api_gateway_rest_api.chatbot_api.id
   resource_id             = aws_api_gateway_resource.chatbot_resource.id
   http_method             = aws_api_gateway_method.chatbot_post.http_method
@@ -88,38 +57,7 @@ resource "aws_api_gateway_integration" "chatbot_integration" {
   uri                     = aws_lambda_function.chatbot_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "chatbot_post_response" {
-  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
-  resource_id = aws_api_gateway_resource.chatbot_resource.id
-  http_method = aws_api_gateway_method.chatbot_post.http_method
-  status_code = "200"
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-}
-
-resource "aws_api_gateway_integration_response" "chatbot_post_integration_response" {
-  depends_on  = [aws_api_gateway_integration.chatbot_integration]
-  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
-  resource_id = aws_api_gateway_resource.chatbot_resource.id
-  http_method = aws_api_gateway_method.chatbot_post.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-  }
-
-  response_templates = {
-    "application/json" = ""
-  }
-}
-
-# OPTIONS method (CORS preflight)
+# OPTIONS method for CORS
 resource "aws_api_gateway_method" "chatbot_options" {
   rest_api_id   = aws_api_gateway_rest_api.chatbot_api.id
   resource_id   = aws_api_gateway_resource.chatbot_resource.id
@@ -132,8 +70,6 @@ resource "aws_api_gateway_integration" "chatbot_options_integration" {
   resource_id             = aws_api_gateway_resource.chatbot_resource.id
   http_method             = aws_api_gateway_method.chatbot_options.http_method
   type                    = "MOCK"
-  passthrough_behavior    = "WHEN_NO_MATCH"
-
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
@@ -145,68 +81,46 @@ resource "aws_api_gateway_method_response" "chatbot_options_response" {
   http_method = aws_api_gateway_method.chatbot_options.http_method
   status_code = "200"
 
-  response_models = {
-    "application/json" = "Empty"
-  }
-
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
     "method.response.header.Access-Control-Allow-Origin"  = true
   }
 }
 
 resource "aws_api_gateway_integration_response" "chatbot_options_integration_response" {
-  depends_on  = [aws_api_gateway_integration.chatbot_options_integration]
   rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
   resource_id = aws_api_gateway_resource.chatbot_resource.id
   http_method = aws_api_gateway_method.chatbot_options.http_method
-  status_code = "200"
+  status_code = aws_api_gateway_method_response.chatbot_options_response.status_code
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key'",
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
+}
 
-  response_templates = {
-    "application/json" = ""
-  }
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.chatbot_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.chatbot_api.execution_arn}/*/*"
 }
 
 resource "aws_api_gateway_deployment" "chatbot_deployment" {
   depends_on = [
-    aws_api_gateway_integration.chatbot_integration,
+    aws_api_gateway_integration.chatbot_post_integration,
     aws_api_gateway_integration.chatbot_options_integration,
-    aws_api_gateway_integration_response.chatbot_options_integration_response,
-    aws_api_gateway_method_response.chatbot_options_response,
     aws_api_gateway_method.chatbot_post,
-    aws_api_gateway_method_response.chatbot_post_response
+    aws_api_gateway_method.chatbot_options
   ]
 
   rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
-
-  triggers = {
-    redeploy = timestamp()
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  stage_name  = "prod"
 }
 
-resource "aws_api_gateway_stage" "chatbot_stage" {
-  deployment_id = aws_api_gateway_deployment.chatbot_deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.chatbot_api.id
-  stage_name    = "prod"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  depends_on = [aws_api_gateway_deployment.chatbot_deployment]
-}
-
-output "chatbot_api_url" {
-  value = "https://${aws_api_gateway_rest_api.chatbot_api.id}.execute-api.eu-west-3.amazonaws.com/${aws_api_gateway_stage.chatbot_stage.stage_name}/chat"
+output "api_url" {
+  value = "${aws_api_gateway_deployment.chatbot_deployment.invoke_url}/chat"
 }
