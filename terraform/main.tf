716b0276172d40320fde8b17d691510a21b88b5e
Grzesiek <grzegorzszuper2@gmail.com>
2025-08-07 19:21:25 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,9 +26,7 @@ resource "aws_iam_role_policy" "bedrock_invoke_policy" {
     Statement = [
       {
         Effect = "Allow",
-        Action = [
-          "bedrock:InvokeModel"
-        ],
+        Action = ["bedrock:InvokeModel"],
         Resource = "*"
       },
       {
@@ -65,7 +63,7 @@ resource "aws_api_gateway_resource" "chatbot_resource" {
   path_part   = "chat"
 }
 
-# POST /chat
+# POST
 resource "aws_api_gateway_method" "chatbot_post" {
   rest_api_id   = aws_api_gateway_rest_api.chatbot_api.id
   resource_id   = aws_api_gateway_resource.chatbot_resource.id
@@ -74,46 +72,15 @@ resource "aws_api_gateway_method" "chatbot_post" {
 }
 
 resource "aws_api_gateway_integration" "chatbot_integration" {
-  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
-  resource_id = aws_api_gateway_resource.chatbot_resource.id
-  http_method = aws_api_gateway_method.chatbot_post.http_method
-
+  rest_api_id             = aws_api_gateway_rest_api.chatbot_api.id
+  resource_id             = aws_api_gateway_resource.chatbot_resource.id
+  http_method             = aws_api_gateway_method.chatbot_post.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.chatbot_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "chatbot_post_response" {
-  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
-  resource_id = aws_api_gateway_resource.chatbot_resource.id
-  http_method = aws_api_gateway_method.chatbot_post.http_method
-  status_code = "200"
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-}
-
-resource "aws_api_gateway_integration_response" "chatbot_post_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
-  resource_id = aws_api_gateway_resource.chatbot_resource.id
-  http_method = aws_api_gateway_method.chatbot_post.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-  }
-
-  response_templates = {
-    "application/json" = ""
-  }
-}
-
-# OPTIONS /chat (CORS)
+# OPTIONS (CORS)
 resource "aws_api_gateway_method" "chatbot_options" {
   rest_api_id   = aws_api_gateway_rest_api.chatbot_api.id
   resource_id   = aws_api_gateway_resource.chatbot_resource.id
@@ -122,37 +89,14 @@ resource "aws_api_gateway_method" "chatbot_options" {
 }
 
 resource "aws_api_gateway_integration" "chatbot_options_integration" {
-  rest_api_id          = aws_api_gateway_rest_api.chatbot_api.id
-  resource_id          = aws_api_gateway_resource.chatbot_resource.id
-  http_method          = aws_api_gateway_method.chatbot_options.http_method
-  type                 = "MOCK"
-  passthrough_behavior = "WHEN_NO_MATCH"
+  rest_api_id             = aws_api_gateway_rest_api.chatbot_api.id
+  resource_id             = aws_api_gateway_resource.chatbot_resource.id
+  http_method             = aws_api_gateway_method.chatbot_options.http_method
+  type                    = "MOCK"
+  passthrough_behavior    = "WHEN_NO_MATCH"
 
   request_templates = {
-    "application/json" = <<EOF
-{
-  "statusCode": 200
-}
-EOF
-  }
-}
-
-resource "aws_api_gateway_integration_response" "chatbot_options_integration_response" {
-  depends_on = [aws_api_gateway_integration.chatbot_options_integration]
-
-  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
-  resource_id = aws_api_gateway_resource.chatbot_resource.id
-  http_method = aws_api_gateway_method.chatbot_options.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-
-  response_templates = {
-    "application/json" = ""
+    "application/json" = "{\"statusCode\": 200}"
   }
 }
 
@@ -167,9 +111,27 @@ resource "aws_api_gateway_method_response" "chatbot_options_response" {
   }
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
     "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "chatbot_options_integration_response" {
+  depends_on  = [aws_api_gateway_integration.chatbot_options_integration]
+  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
+  resource_id = aws_api_gateway_resource.chatbot_resource.id
+  http_method = aws_api_gateway_method.chatbot_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
   }
 }
 
@@ -185,8 +147,8 @@ resource "aws_api_gateway_deployment" "chatbot_deployment" {
   depends_on = [
     aws_api_gateway_integration.chatbot_integration,
     aws_api_gateway_integration.chatbot_options_integration,
-    aws_api_gateway_method_response.chatbot_post_response,
-    aws_api_gateway_integration_response.chatbot_post_integration_response
+    aws_api_gateway_integration_response.chatbot_options_integration_response,
+    aws_api_gateway_method_response.chatbot_options_response
   ]
   rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
 }
