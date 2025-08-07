8532d1bf9de20aa41992bb617baf3d13fc025bb6
Grzesiek <grzegorzszuper2@gmail.com>
2025-08-07 18:00:56 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,9 +91,14 @@ resource "aws_lambda_permission" "allow_api_gateway" {
 }
 
 resource "aws_api_gateway_deployment" "chatbot_deployment" {
-  depends_on = [aws_api_gateway_integration.chatbot_integration]
+  depends_on  = [aws_api_gateway_integration.chatbot_integration]
   rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
-  stage_name  = "prod"
+}
+
+resource "aws_api_gateway_stage" "chatbot_stage" {
+  deployment_id = aws_api_gateway_deployment.chatbot_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.chatbot_api.id
+  stage_name    = "prod"
 }
 
 output "chatbot_api_url" {
