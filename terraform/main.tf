f72b448cf133b1240f30853dabb0b469e7b46869
Grzesiek <grzegorzszuper2@gmail.com>
2025-08-07 20:14:39 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,8 +118,14 @@ resource "aws_api_gateway_deployment" "chatbot_deployment" {
   ]
 
   rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
-  stage_name  = "prod"
 }
+
+resource "aws_api_gateway_stage" "chatbot_stage" {
+  rest_api_id   = aws_api_gateway_rest_api.chatbot_api.id
+  deployment_id = aws_api_gateway_deployment.chatbot_deployment.id
+  stage_name    = "prod"
+}
+
 
 output "api_url" {
   value = "${aws_api_gateway_deployment.chatbot_deployment.invoke_url}/chat"
