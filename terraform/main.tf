41a874923d31c3a231d5a46570b592c090c47e26
Grzesiek <grzegorzszuper2@gmail.com>
2025-08-07 17:59:23 +0200

    API Gateway do Lambdy

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,3 +53,49 @@ resource "aws_lambda_function" "chatbot_lambda" {
   timeout       = 10
   source_code_hash = filebase64sha256("lambda.zip")
 }
+
+resource "aws_api_gateway_rest_api" "chatbot_api" {
+  name        = "chatbot-api"
+  description = "API Gateway for AI chatbot using Claude 3 and Lambda"
+}
+
+resource "aws_api_gateway_resource" "chatbot_resource" {
+  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
+  parent_id   = aws_api_gateway_rest_api.chatbot_api.root_resource_id
+  path_part   = "chat"
+}
+
+resource "aws_api_gateway_method" "chatbot_post" {
+  rest_api_id   = aws_api_gateway_rest_api.chatbot_api.id
+  resource_id   = aws_api_gateway_resource.chatbot_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "chatbot_integration" {
+  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
+  resource_id = aws_api_gateway_resource.chatbot_resource.id
+  http_method = aws_api_gateway_method.chatbot_post.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.chatbot_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.chatbot_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.chatbot_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "chatbot_deployment" {
+  depends_on = [aws_api_gateway_integration.chatbot_integration]
+  rest_api_id = aws_api_gateway_rest_api.chatbot_api.id
+  stage_name  = "prod"
+}
+
+output "chatbot_api_url" {
+  value = "${aws_api_gateway_deployment.chatbot_deployment.invoke_url}/chat"
+}
