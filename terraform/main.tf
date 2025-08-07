a93db9c620983d14f7caff0176c5a705ca11105f
Grzesiek <grzegorzszuper2@gmail.com>
2025-08-07 18:44:23 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -105,21 +105,6 @@ resource "aws_api_gateway_integration" "chatbot_options_integration" {
 }
 EOF
   }
-
-  resource "aws_api_gateway_integration" "chatbot_options_integration" {
-  rest_api_id          = aws_api_gateway_rest_api.chatbot_api.id
-  resource_id          = aws_api_gateway_resource.chatbot_resource.id
-  http_method          = aws_api_gateway_method.chatbot_options.http_method
-  type                 = "MOCK"
-  passthrough_behavior = "WHEN_NO_MATCH"
-
-  request_templates = {
-    "application/json" = <<EOF
-{
-  "statusCode": 200
-}
-EOF
-  }
 }
 
 resource "aws_api_gateway_integration_response" "chatbot_options_integration_response" {
@@ -137,8 +122,6 @@ resource "aws_api_gateway_integration_response" "chatbot_options_integration_res
   response_templates = {
     "application/json" = ""
   }
-}
-
 }
 
 resource "aws_api_gateway_method_response" "chatbot_options_response" {
@@ -167,7 +150,7 @@ resource "aws_lambda_permission" "allow_api_gateway" {
 }
 
 resource "aws_api_gateway_deployment" "chatbot_deployment" {
-  depends_on  = [
+  depends_on = [
     aws_api_gateway_integration.chatbot_integration,
     aws_api_gateway_integration.chatbot_options_integration
   ]
